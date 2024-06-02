6367bcc007e1b767b853b472f7a885e1fa0bf10f
hinamei <hina.gntm@gmail.com>
2024-06-02 18:14:25 +0900

    インデントを4から2に統一

--- 01_地図表示/main.js
+++ 01_地図表示/main.js
@@ -1,6 +1,6 @@
 var map = new maplibregl.Map({
-    container: 'map',
-    style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
-    center: [139.7024, 35.6598], // 中心座標
-    zoom: 16, // ズームレベル
+  container: 'map',
+  style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
+  center: [139.7024, 35.6598], // 中心座標
+  zoom: 16, // ズームレベル
 });
--- 04_ライン表示/main.js
+++ 04_ライン表示/main.js
@@ -1,28 +1,28 @@
 var map = new maplibregl.Map({
-    container: 'map',
-    style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
-    center: [139.7024, 35.6598], // 中心座標
-    zoom: 16, // ズームレベル
+  container: 'map',
+  style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
+  center: [139.7024, 35.6598], // 中心座標
+  zoom: 16, // ズームレベル
 });
 
 // ラインデータを表示する
 map.on('load', function () {
-    map.addSource('tokyo_bus', {
-        type: 'geojson',
-        data: './data/line.geojson',
-    });
-    map.addLayer({
-        id: 'tokyo_bus',
-        type: 'line',
-        source: 'tokyo_bus',
-        layout: {
-            'line-join': 'round',
-            'line-cap': 'round',
-        },
-        paint: {
-            'line-color': '#0067c0',
-            'line-width': 5,
-        },
-        filter: ['==', 'N07_002', '都営バス'],
-    });
+  map.addSource('tokyo_bus', {
+    type: 'geojson',
+    data: './data/line.geojson',
+  });
+  map.addLayer({
+    id: 'tokyo_bus',
+    type: 'line',
+    source: 'tokyo_bus',
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': '#0067c0',
+      'line-width': 5,
+    },
+    filter: ['==', 'N07_002', '都営バス'],
+  });
 });
